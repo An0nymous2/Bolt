8d9fc9528f4013f5347d4f4b9ee3f5dbad17726c
An0nymous <97083513+AgentP5547@users.noreply.github.com>
2024-08-24 11:29:32 +0530

    Update main.py
    Minor Bug fixes.

--- main.py
+++ main.py
@@ -17,24 +17,25 @@ class App:
           UserInput = MessageEntry.get()
 
           TextBox.configure(state="normal")
-          TextBox.insert(ctk.END, f"You - {UserInput}")
+          TextBox.insert(ctk.END, f"You - {UserInput}\n")
           TextBox.configure(state='disabled')
-
-          GeminiResponse = chat_session.send_message(MessageEntry.get()).text
+          MessageEntry.delete(0, ctk.END)
+          
+          GeminiResponse = chat_session.send_message(UserInput).text
+          
 
           TextBox.configure(state="normal")
-          TextBox.insert(ctk.END, f"Bolt - {GeminiResponse}")
+          TextBox.insert(ctk.END, f"Bolt - {GeminiResponse}\n")
           TextBox.configure(state='disabled')
 
-          print("You - ", UserInput)
-          print("Bolt - ",GeminiResponse)
+          
      
     def CreateAppWindow():
         global MessageEntry
         global TextBox
         
         MessageThread = threading.Thread(target=App.SendMessage)
-        def StartMessageaThread():
+        def StartMessageThread():
             MessageThread.start()
 
         root = ctk.CTk()
@@ -42,22 +43,23 @@ class App:
         root.geometry("900x500")
         root.iconbitmap("images/icon.ico")
 
-        TextBox = ctk.CTkTextbox(root, height=400, width=650)
+        TextBox = ctk.CTkTextbox(root, height=400, width=650, wrap=ctk.WORD)
         TextBox.place(x=20, y=20)
         TextBox.configure(state="disabled")
 
         MessageEntry = ctk.CTkEntry(root, width=595, height=35)
         MessageEntry.place(x=20, y=440)
+        MessageEntry.bind("<Return>", lambda event: MessageThread.start())
         
         
         SendButton = ctk.CTkButton(root, text=None, image=App.SendImageIcon, width=10, fg_color="#242424", hover_color="#201D1D",
-                                  corner_radius=5, command=StartMessageaThread)
+                                  corner_radius=5, command=StartMessageThread)
         SendButton.place(x=615, y=435)
         
 
         ### SIDE FRAME START ###
         FunctionsFrame = ctk.CTkFrame(root, height=200, width=200)
-        FunctionsFrame.place(x=700, y=210)
+        FunctionsFrame.place(x=700, y=180)
 
         CopyButton = ctk.CTkButton(FunctionsFrame, text="Copy Response")
         CopyButton.pack(padx=10, pady=5)
